375c685bd16d5c590aeef0350d1363e6470f1fc9
TerryChan2003 <aleksandr_h2001@mail.ru>
2019-12-07 11:57:37 +0300

    fixed EventDrops

--- src/App.js
+++ src/App.js
@@ -1,73 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 //import CollapsibleTree from "./components/CollapsibleTree";
 import Tree from "react-d3-tree";
 import initialData from "./components/CollapsibleTreeData.js";
 import initialRepositories from "./components/EventDrops.json";
-//import EventDrops from "./components/EventDrops.js";
-import EventDrops from "react-boilerplate/app/components/EventDrops/";
+import EventDrops from "./components/EventDrops/index.js";
+
 function App() {
-  const panels = [{ name: "CollapsibleTree" }, { name: "EventDrops" }];
-  let [data, setData] = useState(initialData);
-  let [repositories, setRepositories] = useState(initialRepositories);
-  let [activePanel, setActivePanel] = useState(panels[0].name);
-  useEffect(() => console.log("edited data"), [data]);
-  useEffect(() => console.log("edited repositories", [repositories]));
-  return (
-    <div>
-      {activePanel === panels[0].name && (
+    const panels = [{name: "CollapsibleTree"}, {name: "EventDrops"}];
+    let [data, setData] = useState(initialData);
+    let [repositories, setRepositories] = useState(initialRepositories);
+    let [activePanel, setActivePanel] = useState(panels[0].name);
+    useEffect(() => setRepositories(repositories => repositories.map(r => ({data: r.commits, name: r.name}))), []);
+    useEffect(() => console.log("edited data"), [data]);
+    useEffect(() => console.log("edited repositories", [repositories]));
+    return (
         <div>
-          <button
-            onClick={() =>
-              setData({
-                name: "test",
-                children: [
-                  { name: "IEvaluable", value: 335 },
-                  { name: "IPredicate", value: 383 },
-                  { name: "IValueProxy", value: 874 }
-                ]
-              })
-            }
-          >
-            Изменить
-          </button>
-          <button onClick={() => setActivePanel(panels[1].name)}>
-            Перейти к EventDrops
-          </button>
-          <div style={{ width: window.outerWidth, height: window.outerHeight }}>
-            <Tree data={data} />
-          </div>
+            {activePanel === panels[0].name && (
+                <div>
+                    <button
+                        onClick={() =>
+                            setData({
+                                name: "test",
+                                children: [
+                                    {name: "IEvaluable", value: 335},
+                                    {name: "IPredicate", value: 383},
+                                    {name: "IValueProxy", value: 874}
+                                ]
+                            })
+                        }
+                    >
+                        Изменить
+                    </button>
+                    <button onClick={() => setActivePanel(panels[1].name)}>
+                        Перейти к EventDrops
+                    </button>
+                    <div style={{width: window.outerWidth, height: window.outerHeight}}>
+                        <Tree data={data}/>
+                    </div>
+                </div>
+            )}
+            {activePanel === panels[1].name && (
+                <div>
+                    <button
+                        onClick={() =>
+                            setRepositories([
+                                {
+                                    name: "admin-on-rest",
+                                    data: [{date: new Date("2014/09/15 14:21:31")}]
+                                },
+                                {
+                                    name: "event-drops",
+                                    data: [{date: new Date("2014/09/15 13:24:57")}]
+                                },
+                                {
+                                    name: "sedy",
+                                    data: [{date: new Date("2014/09/15 13:25:12")}]
+                                }
+                            ])
+                        }
+                    >
+                        Изменить
+                    </button>
+                    <button onClick={() => setActivePanel(panels[0].name)}>
+                        Перейти к CollapsibleTree
+                    </button>
+                    <EventDrops events={repositories} style={{width: window.outerWidth, height: window.outerHeight}}/>
+                </div>
+            )}
         </div>
-      )}
-      {activePanel === panels[1].name && (
-        <div>
-          <button
-            onClick={() =>
-              setRepositories([
-                {
-                  name: "admin-on-rest",
-                  data: [{ date: new Date("2014/09/15 14:21:31") }]
-                },
-                {
-                  name: "event-drops",
-                  data: [{ date: new Date("2014/09/15 13:24:57") }]
-                },
-                {
-                  name: "sedy",
-                  data: [{ date: new Date("2014/09/15 13:25:12") }]
-                }
-              ])
-            }
-          >
-            Изменить
-          </button>
-          <button onClick={() => setActivePanel(panels[0].name)}>
-            Перейти к CollapsibleTree
-          </button>
-          <EventDrops data={repositories} height={240} width={775} />
-        </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/EventDrops/index.js
@@ -0,0 +1,163 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as d3 from 'd3';
+import eventDrops from 'event-drops';
+import 'd3-selection';
+
+export const humanizeDate = date => {
+  const monthNames = [
+    'Jan.',
+    'Feb.',
+    'March',
+    'Apr.',
+    'May',
+    'June',
+    'Jul.',
+    'Aug.',
+    'Sept.',
+    'Oct.',
+    'Nov.',
+    'Dec.',
+  ];
+
+  return `
+        ${monthNames[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}
+        ${date.getHours()}:${date.getMinutes()}
+    `;
+};
+
+const updateCommitsInformation = chart => {
+  const filteredData = chart
+    .filteredData()
+    .reduce((total, calendar) => total.concat(calendar.data), []);
+  return {
+    zoomStart: humanizeDate(chart.scale().domain()[0]),
+    zoomEnd: humanizeDate(chart.scale().domain()[1]),
+    data: filteredData,
+  };
+};
+
+export default class EventDrops extends React.PureComponent {
+  static propTypes = {
+    events: PropTypes.any,
+    onDataPointHover: PropTypes.func,
+    onZoom: PropTypes.func,
+    loading: PropTypes.bool,
+    onClick: PropTypes.func
+  };
+
+  constructor(props) {
+    const { loading } = props;
+
+    super(props);
+
+    if (!loading) {
+      this.state = {
+        id: this.generateUniqueId(),
+      };
+      this.chart = this.initializeChart();
+    }
+  }
+
+  generateUniqueId() {
+    return `eventdrops_component_${Math.floor(Math.random() * 10000000)}`;
+  }
+
+  generatePalette(opacity) {
+    return [
+      `rgb(239, 83, 80, ${opacity})`,
+      `rgb(236, 64, 122, ${opacity})`,
+      `rgb(171, 71, 188, ${opacity})`,
+      `rgb(126, 87, 194, ${opacity})`,
+      `rgb(92, 107, 192, ${opacity})`,
+      `rgb(66, 165, 245, ${opacity})`,
+      `rgb(41, 182, 246, ${opacity})`,
+      `rgb(38, 198, 218, ${opacity})`,
+      `rgb(38, 166, 154, ${opacity})`,
+      `rgb(38, 166, 154, ${opacity})`,
+      '#26a69a',
+      '#66bb6a',
+      '#9ccc65',
+      '#d4e157',
+      '#ffee58',
+      '#ffca28',
+      '#ffa726',
+      '#ff7043',
+      '#8d6e63',
+      '#bdbdbd',
+      '#78909c',
+    ];
+  }
+
+  renderChartData = () => {
+    const { events } = this.props;
+    const { id } = this.state;
+    const { chart } = this;
+    const { onDataPointHover } = this.props;
+    if (events && events.length) {
+      const chartD3 = d3
+        .select(`#${id}`)
+        .data([events])
+        .call(chart);
+
+      const palleteTransparent = d3.scaleOrdinal(this.generatePalette(0.35));
+      const palleteOpaque = d3.scaleOrdinal(this.generatePalette(1));
+
+      [
+        { selector: '.drops', pallete: palleteTransparent },
+        { selector: '.line-label', pallete: palleteOpaque },
+        { selector: '.indicator', pallete: d3.scaleOrdinal(['#ffffff4f']) },
+      ].map(({ selector, pallete }) =>
+        chartD3.selectAll(selector).attr('fill', (d, i) => pallete(i)),
+      );
+
+      d3.selectAll('.drop').on('click', function(pointData) {
+        d3.selectAll('.drop').style('stroke', null);
+        d3.select(this)
+          .style('stroke', 'white')
+          .style('stroke-width', '2px');
+        if (onDataPointHover) {
+          onDataPointHover(pointData);
+        }
+      });
+    }
+  };
+
+  initializeChart() {
+    return eventDrops({
+      d3,
+      metaballs: false,
+      zoom: {
+        onZoomEnd: () => {
+          if (this.props.onZoom) {
+            this.props.onZoom(updateCommitsInformation(this.chart));
+          }
+        },
+      },
+      drop: {
+        radius: 15,
+        date: d => new Date(d.date)
+      },
+      numberDisplayedTicks: {
+        small: 3,
+        medium: 4,
+        large: 5,
+        extra: 6,
+      },
+    });
+  }
+
+  componentDidUpdate() {
+    this.renderChartData();
+  }
+
+  componentDidMount() {
+    this.renderChartData();
+  }
+
+  render() {
+    const { id } = this.state;
+    const { ...rest } = this.props;
+    return <div id={id} {...rest} />;
+  }
+}
